d857b841ec4bfc51991224a7e805cfc94c114cfb
Benji Smith <smith.benj@husky.neu.edu>
2017-07-05 20:29:15 -0700

    Initial commit, basically move, spawn pong ball.

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerControl : NetworkBehaviour
+{
+
+	public float speed = 4.0f;
+
+	public GameObject pongBall;
+
+	GameObject pongBallInstance;
+
+	bool gameIsStarted = false;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (hasAuthority)
+		{
+			transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+			//transform.position += Vector3.up * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		}
+
+		if (!gameIsStarted && isServer && connectionToClient.isConnected)
+		{
+			OnStartGame();
+		}
+		// A bit hacky, but good for testing if a client DC's and we want to restart the game
+		else if (gameIsStarted && !connectionToClient.isConnected)
+		{
+			gameIsStarted = false;
+		}
+	}
+
+	void OnStartGame()
+	{
+		gameIsStarted = true;
+		if (pongBallInstance)
+		{
+			Destroy(pongBallInstance);
+		}
+		pongBallInstance = Instantiate(pongBall, Vector3.zero, Quaternion.identity);
+		NetworkServer.Spawn(pongBallInstance);
+	}
+}
---
+++ Assets/PongBall.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PongBall : NetworkBehaviour
+{
+
+	public float speed = 1.0f;
+
+	public float sign = 1.0f;
+
+	float theirY = Mathf.Infinity;
+	float ourY = Mathf.NegativeInfinity;
+
+	void Start()
+	{
+		var players = FindObjectsOfType<PlayerControl>();
+		
+		if (players[0].isLocalPlayer)
+		{
+			ourY = players[0].transform.position.y;
+			theirY = players[1].transform.position.y;
+		}
+		else
+		{
+			theirY = players[0].transform.position.y;
+			ourY = players[1].transform.position.y;
+		}
+
+	}
+
+	void Update () {
+		// The server spawns the pongball,
+		// so it's the one moving it
+		if (isServer)
+		{
+			float minY = Mathf.Min(ourY, theirY);
+			float maxY = Mathf.Max(ourY, theirY);
+			transform.position += Vector3.up * Time.deltaTime * speed * sign;
+
+			if (transform.position.y > maxY)
+			{
+				sign = -1.0f;
+			}
+			else if (transform.position.y < minY)
+			{
+				sign = 1.0f;
+			}
+		}
+	}
+}
