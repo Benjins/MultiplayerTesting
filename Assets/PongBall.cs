f2c1bb76beba81784e9b0e93e313d75de52125d2
Benji Smith <smith.benj@husky.neu.edu>
2017-07-10 19:04:25 -0700

    Ball now detects if it hits a paddle, and resets.

--- Assets/PongBall.cs
+++ Assets/PongBall.cs
@@ -10,6 +10,9 @@ public class PongBall : NetworkBehaviour
 
 	public float sign = 1.0f;
 
+	// players[0] = us, players[1] = them
+	PlayerControl[] players;
+
 	// For now, the ball mostly travels back and forth on the X axis,
 	// not including potential up/down dips
 	float theirX = Mathf.Infinity;
@@ -18,23 +21,36 @@ public class PongBall : NetworkBehaviour
 	float minY = 0.0f;
 	float maxY = 0.0f;
 
+	float zVelocity = 0.0f;
+
+	Vector3 startPos;
+
 	void Start()
 	{
-		var players = FindObjectsOfType<PlayerControl>();
-		
-		if (players[0].isLocalPlayer)
+		players = FindObjectsOfType<PlayerControl>();
+
+		startPos = transform.position;
+
+		// If players[0] is not local, swap them
+		if (!players[0].isLocalPlayer)
 		{
-			ourX = players[0].transform.position.x;
-			theirX = players[1].transform.position.x;
+			PlayerControl temp = players[0];
+			players[0] = players[1];
+			players[1] = temp;
 		}
-		else
-		{
-			theirX = players[0].transform.position.x;
-			ourX = players[1].transform.position.x;
-		}
+
+		ourX = players[0].transform.position.x;
+		theirX = players[1].transform.position.x;
 
 		// TODO: Ensure that they're at the same height?
 		maxY = players[0].transform.position.y;
+	}
+
+	void Reset()
+	{
+		sign = 1.0f;
+		zVelocity = 0.0f;
+		transform.position = new Vector3((ourX + theirX) / 2, players[0].transform.position.y, 0.0f);
 	}
 
 	void Update () {
@@ -58,11 +74,30 @@ public class PongBall : NetworkBehaviour
 			if (transform.position.x > maxX)
 			{
 				sign = -1.0f;
+				HandlePaddle((ourX == maxX) ? players[0] : players[1]);
+				
 			}
 			else if (transform.position.x < minX)
 			{
 				sign = 1.0f;
+				HandlePaddle((ourX == minX) ? players[0] : players[1]);
 			}
+		}
+	}
+
+	void HandlePaddle(PlayerControl player)
+	{
+		float ballZ = transform.position.z;
+		float minZ = player.transform.position.z - player.transform.localScale.z;
+		float maxZ = player.transform.position.z + player.transform.localScale.z;
+
+		if (ballZ < minZ || ballZ > maxZ)
+		{
+			Reset();
+		}
+		else
+		{
+			zVelocity = 0.0f;
 		}
 	}
 }
